b816e024563e588c747d48f4de8ad7077c4dc931
Kinga Kohari <kinguca@yahoo.com>
2022-01-28 11:42:50 +0100

    Read more button created

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ const server = http.createServer((req, res) => {
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="preconnect" href="https://fonts.googleapis.com"> 
 		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
-		<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@800&display=swap" rel="stylesheet">
+		<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@600&display=swap" rel="stylesheet">
 		<style>
 			body{
 				padding: 0; margin: 0;
